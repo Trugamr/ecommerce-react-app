1b992524e84ca841999118e7f6d1c88f71fd56d4
Trugamr <6trugamr9@gmail.com>
2020-03-15 19:35:24 +0530

    Updates

---
+++ precache-manifest.6c49ac84b7a115e9c3f078c7186748f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f23dd7b3ad669bd682bf2ae5b9a04a02",
+    "url": "/ecommerce-react-app/index.html"
+  },
+  {
+    "revision": "1719d31b826a02ab26b0",
+    "url": "/ecommerce-react-app/static/css/main.cdd750a5.chunk.css"
+  },
+  {
+    "revision": "5c0923e3a8fa30494497",
+    "url": "/ecommerce-react-app/static/js/2.acd69210.chunk.js"
+  },
+  {
+    "revision": "6985d75ffd76ce4f1484f26e7c7f1ad2",
+    "url": "/ecommerce-react-app/static/js/2.acd69210.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1719d31b826a02ab26b0",
+    "url": "/ecommerce-react-app/static/js/main.e0c7346a.chunk.js"
+  },
+  {
+    "revision": "5d32e5f6abf2875e0083",
+    "url": "/ecommerce-react-app/static/js/runtime-main.71022f27.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/ecommerce-react-app/static/media/logo.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/ecommerce-react-app/static/media/shopping-bag.30f07071.svg"
+  }
+]);
