081538aa7d3d17b1ef93a96f79c05fb14795917d
Trugamr <6trugamr9@gmail.com>
2020-03-18 21:28:58 +0530

    created separate container components for collection overview and collection page, using redux-thunk for async fetching of collections from firebase

---
+++ src/pages/collection/collection.container.jsx
@@ -0,0 +1,19 @@
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
+import { compose } from 'redux'
+
+import { selectIsCollectionsLoaded } from '../../redux/shop/shop.selectors'
+
+import CollectionPage from './collection.component'
+import withSpinner from '../../components/with-spinner/with-spinner.component'
+
+const mapStateToProps = createStructuredSelector({
+  isLoading: state => !selectIsCollectionsLoaded(state)
+})
+
+const CollectionPageContainer = compose(
+  connect(mapStateToProps),
+  withSpinner
+)(CollectionPage)
+
+export default CollectionPageContainer
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -3,60 +3,31 @@ import { connect } from 'react-redux'
 import { Route } from 'react-router-dom'
 import './shop.styles.scss'
 
-import {
-  firestore,
-  convertCollectionsSnapshotToMap
-} from '../../firebase/firebase.utils'
+import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container'
+import CollectionPageContainer from '../collection/collection.container'
 
-import withSpinner from '../../components/with-spinner/with-spinner.component'
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
-import CollectionPage from '../collection/collection.component'
-
-import { updateCollections } from '../../redux/shop/shop.actions'
-
-const CollectionsOverviewWithSpinner = withSpinner(CollectionsOverview)
-const CollectionPageWithSpinner = withSpinner(CollectionPage)
+import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions'
 
 class ShopPage extends React.Component {
-  state = {
-    loading: true
-  }
-
-  unsubscribeFromSnapshot = null
-
   componentDidMount() {
-    const { updateCollections } = this.props
-
-    const collectionsRef = firestore.collection('collections')
-    this.unsubscribeFromSnapshot = collectionsRef.onSnapshot(async snapshot => {
-      const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
-      updateCollections(collectionsMap)
-      this.setState({ loading: false })
-    })
-  }
-
-  componentWillUnmount() {
-    this.unsubscribeFromSnapshot()
+    const { fetchCollectionsStartAsync } = this.props
+    fetchCollectionsStartAsync()
   }
 
   render() {
+    console.log(this.props)
     const { match } = this.props
-    const { loading } = this.state
 
     return (
       <div className="shop-page">
         <Route
           exact
           path={`${match.path}`}
-          render={props => (
-            <CollectionsOverviewWithSpinner isLoading={loading} {...props} />
-          )}
+          component={CollectionsOverviewContainer}
         />
         <Route
           path={`${match.path}/:collectionId`}
-          render={props => (
-            <CollectionPageWithSpinner isLoading={loading} {...props} />
-          )}
+          component={CollectionPageContainer}
         />
       </div>
     )
@@ -64,8 +35,7 @@ class ShopPage extends React.Component {
 }
 
 const mapDispatchToProps = dispatch => ({
-  updateCollections: collectionsMap =>
-    dispatch(updateCollections(collectionsMap))
+  fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
 })
 
 export default connect(null, mapDispatchToProps)(ShopPage)
