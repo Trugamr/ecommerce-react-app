dbd547ed9da91e3e05985fdf381c4b52cc4fe2e9
Trugamr <6trugamr9@gmail.com>
2020-03-27 19:06:42 +0530

    added method to send stripe token to backend

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 import './stripe-button.styles.scss'
 
 import StripeCheckout from 'react-stripe-checkout'
@@ -8,7 +9,22 @@ const StripeButton = ({ price }) => {
   const publishableKey = 'pk_test_Vh1lrnpCcP6oUBYxb3UnS9wP00zvCGkEG4'
 
   const onToken = token => {
-    console.log(token)
+    const baseURL = 'https://tru-ecommerce-api.herokuapp.com'
+
+    axios
+      .post(`${baseURL}/payment`, {
+        amount: priceForStripe,
+        token
+      })
+      .then(response => {
+        alert('Payment Successful')
+      })
+      .catch(error => {
+        console.log('Payment error', error)
+        alert(
+          'There was an issue with your payment. Please make sure you use provided credit card.'
+        )
+      })
   }
 
   return (
